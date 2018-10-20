3dac00c6efd87f73f7b2cca08cf13d513cf2cbd4
Robert Chen <robertchen234@gmail.com>
2018-10-20 14:39:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase? == false
+  elsif phrase == phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   else
     "NO, NOT SINCE 1938!"
